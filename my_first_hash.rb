99ee5a4bb631e7dbc9de8592ac0500f28c21070f
sensible-partial-6157 <daniyellaharmon@gmail.com>
2020-09-21 23:31:23 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,28 +1,21 @@
 def my_hash
-  drinks = {"soda" => "coke", "water" => "sams", "juice" => "grape"}
+  my_hash = {"soda" => "Coke", "juice" => "Grape", "tea" => "Pomegranite"}
 end
 
 
 def shipping_manifest
-  the_manifest = {"whale bone corsets" => "5", "porcelain vases" => "2", "oil painting" => "3"}
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => "5", 
-                  "porcelain vases" => "2", 
-                  "oil paintings" => "3"
-                 }
-shipping_manifest ["oil paintings"]
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  shipping_manifest["oil paintings"]
 end
 
 def adding
-  shipping_manifest = {
-                  "whale bone corsets" => "5", 
-                  "porcelain vases" => "2", 
-                  "oil paintings" => "3"
-   shipping_manifest["muskets"] = "2"
- shipping_manifest["gun powder"] = "4"
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
- shipping_manifest
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4
+  shipping_manifest
 end
